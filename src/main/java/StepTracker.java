feb87b1129ae43fdc6301fdeabf9773fde5d6e17
cassylee-7 <cassandrasyllee@gmail.com>
2025-04-25 22:33:11 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,32 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+ private int totalSteps;
+ private int totalDays;
+ private int actDays;
+ private int minSteps;
+
+ public StepTracker (int goals) {
+  minSteps = goals;
+  totalSteps = 0;
+  totalDays = 0;
+  actDays = 0;
+ }
+
+ public void addDailySteps (int steps) {
+  totalSteps += steps;
+  totalDays++;
+  if (steps >= minSteps) 
+   actDays++;
+ }
+
+ public int activeDays() {
+  return actDays;
+ }
+
+ public double averageSteps() {
+  if (total == 0)
+   return 0.0;
+  return (double) totalSteps/totalDays;
+ }
 } 
